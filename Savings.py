86e408c485bdd2c83074505ad80855153b1b3098
JohnAsare <johnasare7@gmail.com>
2018-09-11 14:37:19 -0400

    " Wrote varialbles to hold the starting amount and the earning amount of the account"

--- Savings.py
+++ Savings.py
@@ -5,11 +5,18 @@
 
 """ A short code to assume how much an account will grown with a fixed APR interest """
 
-Money_to_start = int(input("How much money do you want to start with?: "))
+money_to_start = float(input("How much money do you want to start with?: "))
 
-APR = float(input("(i.e 40% will be .04 or 0.04 What is the APR?:  )"))
+APR = float(input("(i.e 40% will be .04 or 0.04) What is the APR?: "))
 
-Amount = int(input("What amount do you want to get?: "))
+amount = float(input("What amount do you want to get?: "))
 
-Next_year = Amount * (1 + APR)
+year = 0
+
+while money_to_start < amount:
+    money_to_start = money_to_start * (1 + APR)
+    year = year + 1
+    print("after year", year, "the amount is at", round(money_to_start))
+
+
 
